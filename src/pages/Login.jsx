0a18107dec1b4e57db3a55b84f5d7a8360e73a8b
Danilo <dan_argolo@hotmail.com>
2022-11-04 02:33:13 -0300

    02 - Adiciona login, loading e redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    isDisabled: true,
+    loading: false,
+    redirect: false,
+  };
+
+  onInputChange = (event) => {
+    const { name, value } = event.target;
+    const minLength = 3;
+    this.setState({ [name]: value }, () => (
+      value.length >= minLength
+        ? this.setState({ isDisabled: false })
+        : this.setState({ isDisabled: true })
+    ));
+  };
+
+  submitNameUser = async () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name });
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  };
+
   render() {
+    const { name, isDisabled, loading, redirect } = this.state;
+
+    if (loading) { return <Loading />; }
+    if (redirect) { return <Redirect to="/search" />; }
+
     return (
-      <div
-        data-testid="page-login"
-      />
+      <div>
+        <div
+          data-testid="page-login"
+        >
+          <label htmlFor="name">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              id="name"
+              name="name"
+              value={ name }
+              onChange={ this.onInputChange }
+            />
+            Login
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isDisabled }
+            onClick={ this.submitNameUser }
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
     );
   }
 }
