55313e07e559a51d08c36c38fbc5c2e29f0a8484
Danilo <dan_argolo@hotmail.com>
2022-11-04 18:04:05 -0300

    03 - Adiciona Header, consome getUser e loading

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Header extends React.Component {
+  state = {
+    loading: true,
+    name: '',
+  };
+
+  loadUserData = async () => {
+    const userData = await getUser();
+    this.setState({
+      loading: false,
+      name: userData.name });
+  };
+
+  render() {
+    const { loading, name } = this.state;
+    this.loadUserData();
+    if (loading) { return <Loading />; }
+
+    return (
+      <header
+        data-testid="header-component"
+      >
+        <p>Trybe Tunes</p>
+        <span
+          data-testid="header-user-name"
+        >
+          { `Seja bem-vindo ${name}.` }
+        </span>
+      </header>
+
+    );
+  }
+}
+
+export default Header;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Profile extends React.Component {
   render() {
     return (
-      <div
-        data-testid="page-profile"
-      />
+      <>
+        <Header />
+        <div
+          data-testid="page-profile"
+        />
+      </>
     );
   }
 }
